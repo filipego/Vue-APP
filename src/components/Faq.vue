3b325417bd9cc9dd3d86f4cf6ad6d9ffa8041110
Filipe Valente Gomes <filipego@Filipes-MBP.fios-router.home>
2017-08-14 21:24:49 -0400

    seted up vue project

---
+++ src/components/Faq.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="faq">
+    Faq
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Faq',
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style lang="sass" scoped>
+@import '../mq'
+
+
+</style>
